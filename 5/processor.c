25b2f924e4c6dfdc448dbf10b90480b4402aeb82
Roman <ivndonat@yandex.ru>
2020-02-08 19:08:47 +0300

    finished list algorithm (with debug)

--- 5/processor.c
+++ 5/processor.c
@@ -127,21 +127,32 @@ void simulate_processing_list(request_params *params)
 
     // ------------------------------------- MAIN PART OF ALGORITHM
 
-    float time_T1 = 0;
+    float time_T1 = 0; // Time of latest request's arrival in queue
     float time_T2 = 0;
 
-    float time_processing;
-    float time_total_processing = 0;
+    int priority_changes = 1;
+    int max_size = 0;
+
+    float avg_len_T1 = 0;
+    float avg_len_T2 = 0;
+    // Used to store arrival time for cases where both queues are empty when processing ends
+    //float tmp_next_T1;
+    //float tmp_next_T2;
+
+    float time_processing; // Time request spends in processor
+    float time_total_processing = 0; // Time of latest request's entering into processor + processing time
+
     float time_idle = 0;
 
-    int priority;
-    int warning_counter = 0;
+    int priority; // Flag to determine which queue is being processed
+    //int priority_check_flag; // Used to determine if any request arrived into empty queue
+    //int to_check = 0; // Flag to determine if we need to check queues for priority switch
+    
+    int warning_counter = 0; // Debug only
 
+    // Initial data set-up
     time_T1 += gen_number(params->in_time_1_min, params->in_time_1_max);
     time_T2 += gen_number(params->in_time_2_min, params->in_time_2_max);
-
-    float tmp_time_T1 = time_T1;
-    float tmp_time_T2 = time_T2;
 
     // initializing
     if (time_T1 < time_T2)
@@ -159,107 +170,140 @@ void simulate_processing_list(request_params *params)
         priority = PRIORITY_T2;
     }
 
-    printf("Initial T1 size: %d\nInitial T2 size: %d\n", q_list_T1->curr_size, q_list_T2->curr_size);
-
-    printf("T1 arrives at %f\n", time_T1);
-    printf("T2 arrives at %f\n", time_T2);
+    //printf("T1 arrives at %f\n", time_T1);
+    //printf("T2 arrives at %f\n", time_T2);
 
     push_list(q_list_T1, 'a');
     push_list(q_list_T2, 'a');
+
+    int shown_flag = FALSE;
     
-    printf("Current T1 size: %d\nCurrent T2 size: %d\n", q_list_T1->curr_size, q_list_T2->curr_size);
-
-    printf("initialized..\n");
-
-    while (q_list_T1->out < 1000)
+    while (q_list_T1->out <= 1000)
     {
-        // Testing only
-        warning_counter++;
-        if (warning_counter > 1000)
-            break;
+        if (q_list_T1->out % 100 == 0 && q_list_T1->out > 0 && shown_flag == FALSE)
+        {
+            print_queue_list_status(q_list_T1, q_list_T2, q_list_T1->out, avg_len_T1 / priority_changes, 
+                                                                          avg_len_T2 / priority_changes);
+            shown_flag = TRUE;
+        }
+        else
+            shown_flag = FALSE;
 
-        printf("\n\n");
+        //printf("\n\n");
 
         //pause();
         if (priority == PRIORITY_T1)
         {
             pop_list(q_list_T1);
-            printf("T1 left queue at %f\n\n", time_T1);
+            //printf("T1 left queue at %f\n\n", time_total_processing);
          
             time_processing = gen_number(params->process_time_1_min, params->process_time_1_max);
-            time_total_processing = time_processing + tmp_time_T1;
+            time_total_processing += time_processing;
 
-            printf("T1 left processor at %f\n", time_T1 + time_processing);
-            printf("Current total processing time: %f\n\n", time_total_processing);
-            printf("Current T1 size: %d\nCurrent T2 size: %d\n", q_list_T1->curr_size, q_list_T2->curr_size);
+            //printf("T1 left processor after %f\n", time_processing);
+            //printf("Current total processing time: %f\n\n", time_total_processing);
         }
 
         if (priority == PRIORITY_T2)
         {
             pop_list(q_list_T2);
-            printf("T2 left queue at %f\n\n", time_T2);
+            //printf("T2 left queue at %f\n\n", time_total_processing);
          
             time_processing = gen_number(params->process_time_2_min, params->process_time_2_max);
-            time_total_processing = time_processing + tmp_time_T2;
+            time_total_processing += time_processing;
          
-            printf("T2 left processor at %f\n", time_T2 + time_processing);
-            printf("Current total processing time: %f\n\n", time_total_processing);
-            printf("Current T1 size: %d\nCurrent T2 size: %d\n", q_list_T1->curr_size, q_list_T2->curr_size);
-        }
-
-        // Priority switch
-        if (priority == PRIORITY_T1 && is_empty_list(q_list_T1) && time_total_processing < time_T1)
-            if (time_T2 < time_T1)
-            {
-                priority = PRIORITY_T2;
-                printf("switching priority to T2\n");
-            }
-
-        if (priority == PRIORITY_T2 && is_empty_list(q_list_T2) && time_total_processing < time_T2)
-            if (time_T1 < time_T2)
-            {
-                priority = PRIORITY_T1;
-                printf("switching priority to T1\n");
-            }
-
-        // counting idle time
-        if (time_total_processing < time_T2 && time_total_processing < time_T1 && 
-                            is_empty_list(q_list_T1) && is_empty_list(q_list_T2))
-        {
-            if (time_T1 > time_T2)
-            {
-                time_idle += time_T2 - time_total_processing;
-                printf("processor stays idle for %f\n", time_T2 - time_total_processing);
-            }
-            else
-            {
-                time_idle += time_T1 - time_total_processing;
-                printf("processor stays idle for %f\n", time_T1 - time_total_processing);
-            }
+            //printf("T2 left processor after %f\n", time_processing);
+            //printf("Current total processing time: %f\n\n", time_total_processing);
         }
 
         while (time_T1 < time_total_processing)
         {
-            tmp_time_T1 = gen_number(params->in_time_1_min, params->in_time_1_max);
-            time_T1 += tmp_time_T1;
+            time_T1 += gen_number(params->in_time_1_min, params->in_time_1_max);
+
+            // No requests arrived during processing of the last element
+            if (is_empty_list(q_list_T1) && time_T1 > time_total_processing && priority == PRIORITY_T1)
+            {
+                if (!is_empty_list(q_list_T2))
+                {
+                    priority = PRIORITY_T2;
+                    avg_len_T1 += max_size;
+                    max_size = q_list_T2->curr_size;
+                    priority_changes++;
+                    //printf("Priority switched to T2\n");
+                }
+            }
+
             push_list(q_list_T1, 'a');
-            printf("T1 request arrives at %f\n", time_T1);
-            printf("Current T1 size: %d\nCurrent T2 size: %d\n", q_list_T1->curr_size, q_list_T2->curr_size);
+            //printf("T1 request arrives at %f\n", time_T1);
         }
 
         while (time_T2 < time_total_processing)
         {
-            tmp_time_T2 = gen_number(params->in_time_2_min, params->in_time_2_max);
-            time_T2 += tmp_time_T2;
+            time_T2 += gen_number(params->in_time_2_min, params->in_time_2_max);
+            
+            if (is_empty_list(q_list_T2) && time_T2 > time_total_processing && priority == PRIORITY_T2)
+            {
+                if (!is_empty_list(q_list_T1))
+                {
+                    priority = PRIORITY_T1;
+                    avg_len_T2 += max_size;
+                    max_size = q_list_T1->curr_size;
+                    priority_changes++;
+                    //printf("Priority switched to T1\n");
+                }
+            }
+
             push_list(q_list_T2, 'a');
-            printf("T2 request arrives at %f\n", time_T2);
-            printf("Current T1 size: %d\nCurrent T2 size: %d\n", q_list_T1->curr_size, q_list_T2->curr_size);
         }
 
-        // Statistics
+        // Requests arrived into empty queues only after processing
+        // We determine which arrives faster to find out priority
+        if (q_list_T1->curr_size == 1 && q_list_T2->curr_size == 1)
+        {
+            if (time_T1 > time_T2 && priority == PRIORITY_T1)
+            {
+                priority = PRIORITY_T2;
+                avg_len_T1 += max_size;
+                max_size = q_list_T2->curr_size;
+                priority_changes++;
+                //printf("Priority switched to T2\n");
+            }
+            else if (priority == PRIORITY_T2)
+            {
+                priority = PRIORITY_T1;
+                avg_len_T2 += max_size;
+                max_size = q_list_T1->curr_size;
+                priority_changes++;
+                //printf("Priority switched to T1\n");
+            }
+        }   
+        
+        if (priority == PRIORITY_T1 && q_list_T1->curr_size == 1)
+        {
+            if (time_total_processing < time_T1)
+            {
+                //printf("Processor stays idle for %f\n", time_T1 - time_total_processing);
+                time_idle += time_T1 - time_total_processing;
+                time_total_processing = time_T1;
+            }
+        }
+        
+        if (priority == PRIORITY_T2 && q_list_T2->curr_size == 1)
+        {
+            if (time_total_processing < time_T2)
+            {
+                //printf("Processor stays idle for %f\n", time_T2 - time_total_processing);
+                time_idle += time_T2 - time_total_processing;
+                time_total_processing = time_T2;
+            }
+        }
+
+        // Statistics (debug only probably)
+        /*
         printf("\n---------------------------------------------\n");
-        //printf("Current T1 size: %d\nCurrent T2 size: %d\n", q_list_T1->curr_size, q_list_T2->curr_size);
-        //printf("Next T1 request: %f\n Next T2 request: %f\n", time_T1, time_T2);
+        printf("Current T1 size: %d\nCurrent T2 size: %d\n", q_list_T1->curr_size, q_list_T2->curr_size);
+        printf("Last queued T1 request: %f\n", time_T1);
+        printf("Last queued T2 request: %f\n", time_T2);
         printf("Current processing time: %f\n", time_total_processing);
         printf("Total idle time is %f\n", time_idle);
         printf("-----------------------------------------------\n");
@@ -267,20 +311,11 @@ void simulate_processing_list(request_params *params)
         printf("|\n");
         printf("|\n");
         printf("|\n");
-        pause();
-
-        // Testing only
-        if (q_list_T1->curr_size > 100 || q_list_T2->curr_size > 100)
-            break;
+        pause();*/
     }
 
-    // ------------------------------------- END OF ALGORITHM PART
-
     free_list(q_list_T1);
-    free(q_list_T1);
     free_list(q_list_T2);
-    free(q_list_T2);
-
     pause();
 }
 
@@ -308,4 +343,9 @@ void pause()
 void clear()
 {    
     while (getchar() != '\n');
+}
+
+void list_avg_len_update(float curr_len)
+{
+
 }
